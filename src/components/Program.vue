a7b37cb4c10cb23da4fe8909b851316933f98607
ShiftyGOSU <tom_karlsson94@hotmail.com>
2021-03-22 14:19:54 +0100

    Add right now feature

---
+++ src/components/Program.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="programcontainer border">
+    <div class="timecontainer border">
+      <p>{{time}}</p>
+    </div>
+    <program-container :title="title" :description="description" />
+    <div class="stationimg border">
+      <img :src="imgSrc" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProgramContainer from "./ProgramDescription"
+
+export default {
+  name: "Program",
+  props: {
+      title: String,
+      description: String,
+      time: String,
+      imgSrc: String,
+  },
+  components: {
+      ProgramContainer
+  }
+}
+</script>
+
+
+<style scoped>
+.programcontainer {
+  display: flex;
+}
+
+.timecontainer {
+  display: flex;
+  align-items: center;
+  margin: 1em;
+}
+
+.timecontainer > p {
+  margin: 1em;
+}
+
+.stationimg > img {
+  height: 100px;
+  width: 100px;
+  margin: 1em;
+}
+</style>
--- src/components/RightNow.vue
+++ src/components/RightNow.vue
@@ -1,89 +1,172 @@
 <template>
   <div class="rightnow">
-    <div class="leftbox">
-      <div class="lbtitle">
-        Här kan du visa vad som <br />
-        sänds just nu i de olika <br />
-        kanalerna.
+    <div id="container" class="border">
+      <div id="left" class="border">
+        <info-text />
+        <div id="leftbottom" class="border">
+          <radio-station station="p1" id="132" />
+          <radio-station station="p2" id="163" />
+          <radio-station station="p3" id="164" :checked="true" />
+          <radio-station station="p4" />
+        </div>
       </div>
-
-      <div class="channels">
-        <!-- channels here -->
-      </div>
-    </div>
-
-    <div class="rightbox">
-      <div class="rbtitle">Start</div>
-
-      <div>
-        <apexchart
-          width="500"
-          type="bar"
-          :options="chartOptions"
-          :series="series"
-        ></apexchart>
+      <div id="right" class="border">
+        <div id="righttop">
+          <h3 class="textAlignLeft">Start</h3>
+        </div>
+        <div id="rightbottom">
+          <program
+            v-for="program in allPrograms"
+            :key="program"
+            :time="startTime(program.starttimeutc)"
+            :title="program.title"
+            :description="program.description"
+            :imgSrc="program.channel.name + '.png'"
+          >
+            {{ program }}
+          </program>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import VueApexCharts from "vue3-apexcharts";
+import InfoText from "./InfoText";
+import RadioStation from "./RadioStation";
+import Program from "./Program";
+
 export default {
-  components: {
-    apexchart: VueApexCharts,
-  },
-  name: "Right now",
+  name: "RightNow",
   data() {
     return {
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          categories: ["P1", "P2"],
-        },
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40],
-        },
-      ],
+      allPrograms: [],
     };
   },
+  components: {
+    InfoText,
+    RadioStation,
+    Program,
+  },
   methods: {
-    //metoder för att fetcha från api m.m
+    checkTime(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    },
+
+    startTime(dateString) {
+      let timestamp = +dateString.replace(/\/Date\((.*?)\)\//g, "$1");
+      let programStartTime = new Date(timestamp);
+      let h = programStartTime.getHours();
+      let m = programStartTime.getMinutes();
+      // add a zero in front of numbers<10
+      h = this.checkTime(h);
+      m = this.checkTime(m);
+      return h + ":" + m;
+    },
+
+    async getTablo(api) {
+      let json = null;
+
+      try {
+        let response = await fetch(api);
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        json = await response.json();
+      } catch (err) {
+        console.error(err);
+      }
+
+      json = json.schedule;
+
+      let dateNow = new Date();
+
+      for (let i = json.length - 1; i >= 0; i--) {
+        let timestamp = +json[i].endtimeutc.replace(/\/Date\((.*?)\)\//g, "$1");
+        let dateProgram = new Date(timestamp);
+        if (dateNow > dateProgram) {
+          json.splice(i, 1);
+        }
+      }
+
+      Array.prototype.push.apply(this.allPrograms, json);
+
+      this.allPrograms = this.allPrograms.sort(function (a, b) {
+        if (a.starttimeutc > b.starttimeutc) {
+          return 1;
+        } else if (a.starttimeutc < b.starttimeutc) {
+          return -1;
+        }
+        if (a.channel.name > b.channel.name) {
+          return 1;
+        } else if (a.channel.name < b.channel.name) {
+          return -1;
+        }
+        return 0;
+      });
+    },
+
+    removeProgramsFromList(id) {
+      id = Number(id);
+      for (let i = this.allPrograms.length - 1; i >= 0; i--) {
+        if (this.allPrograms[i].channel.id === id) {
+          this.allPrograms.splice(i, 1);
+        }
+      }
+    },
+  },
+  mounted() {
+    this.getTablo(
+      "http://api.sr.se/api/v2/scheduledepisodes?channelid=164&size=500&format=json"
+    );
+    console.log(this.allPrograms);
   },
 };
 </script>
 
 
 <style scoped>
-.rightnow {
-  display: grid;
-  background-color: rgb(192, 181, 221);
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-  margin-left: 7em;
-  margin-right: 7em;
+.textAlignLeft {
+  margin-left: 2em;
 }
 
-.lbtitle {
-  background-color: rgb(148, 123, 155);
+#container {
+  display: flex;
 }
 
-.channels {
+#left {
   display: flex;
   flex-direction: column;
-  margin-left: 3em;
-  margin-right: 3em;
-  margin-top: 1em;
+  flex-grow: 1;
 }
 
-p {
-  font-weight: bolder;
-  font-size: xx-large;
-  margin-top: 0em;
+#lefttop {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#leftbottom {
+  display: grid;
+  margin: 1em;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 1em;
+}
+
+#right {
+  flex-grow: 1;
+}
+
+#righttop {
+  margin-right: auto;
+}
+
+#rightbottom {
+  max-height: 1000px;
+  overflow-y: auto;
 }
 </style>
