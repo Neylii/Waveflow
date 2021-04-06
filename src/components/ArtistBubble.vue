1b7d667674809ef2e5d3c881a0b28c93ab386513
Nixinhas <nikjoh2001@skola.goteborg.se>
2021-04-06 12:07:46 +0200

    fixing scaling on different screen sizes

--- src/components/ArtistBubble.vue
+++ src/components/ArtistBubble.vue
@@ -14,15 +14,55 @@ export default {
 
 <style>
 #bubble {
-    position: absolute;
+    position: relative;
     display: flex;
-    width: 14em;
-    height: 13.5em;
-    left: 13em;
+    width: 10em;
+    height: 10em;
+    top: 2em;
+    left: 5em;
     border-radius: 50%;
     justify-content: center;
     align-items: center;
-    background: linear-gradient(180deg, #31a1bd 0%, rgba(49, 161, 189, 0) 100%);
+    background: linear-gradient(180deg, #31a1bd 0%, rgba(11, 40, 48, 0.418) 100%);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+div + #bubble {
+    width: 8.5em;
+    height: 8.5em;
+    left: -3.5em;
+    top: -3em;
+    background: linear-gradient(180deg, rgba(0, 88, 104, 0.69) 0%, #145f6d 100%);
+}
+
+div + #bubble + #bubble {
+    width: 7.5em;
+    height: 7.5em;
+    left: 4.5em;
+    top: -6em;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%), #84c1cc;
+}
+
+@media screen and (min-width: 768px) {
+    #bubble {
+        width: 13em;
+        height: 13em;
+        top: 10em;
+        left: 4em;
+    }
+
+    div + #bubble {
+        width: 11em;
+        height: 11em;
+        left: -6em;
+        top: 5em;
+    }
+
+    div + #bubble + #bubble {
+        width: 9em;
+        height: 9em;
+        left: 4.5em;
+        top: 0em;
+    }
 }
 </style>
