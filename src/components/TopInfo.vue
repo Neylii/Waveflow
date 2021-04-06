a3f441def8ed32b82e279de5edbeadfe4c810a30
Emma Fredriksson <emfr87@gmail.com>
2021-04-06 11:25:17 +0200

    Fix component for top info

---
+++ src/components/TopInfo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="topinfo">
+        <div class="sectiontitle">
+            <sectiontitle :msg="title" />
+        </div>
+        <div class="infotext">
+            <infobox :msg="infotext" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Infobox from "./Infobox.vue"
+import Sectiontitle from "./Sectiontitle.vue"
+export default {
+    name: "TopInfo",
+    props: {
+        title: String,
+        infotext: String,
+    },
+    components: {
+        Sectiontitle,
+        Infobox,
+    },
+}
+</script>
+
+<style scoped>
+.topinfo {
+    display: flex;
+    flex-direction: row;
+    min-height: 20%;
+    transform: scale(0.8);
+}
+.infotext {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin-left: 0.5em;
+}
+
+@media screen and (min-width: 576px) {
+    .topinfo {
+        transform: scale(1);
+    }
+}
+
+@media screen and (min-width: 700px) {
+    .topinfo {
+        display: grid;
+    }
+
+    .infotext {
+        transform: scale(0.9);
+    }
+}
+</style>
