6793113db2a45909ce73537df830fef0bc21d286
ShiftyGOSU <tom_karlsson94@hotmail.com>
2021-04-10 15:40:03 +0200

    Add JSDoc

--- src/views/RightNow.vue
+++ src/views/RightNow.vue
@@ -50,29 +50,48 @@ export default {
         Program,
     },
     methods: {
-        addZerosToSingleDigits(i) {
-            if (i < 10) {
-                i = "0" + i
+        addZerosToSingleDigits(number) {
+            if (number < 10) {
+                number = "0" + number
             }
-            return i
+            return number
         },
 
+        /**
+         * returns date from jsonDate
+         *
+         * @param {String} dateString The date string to convert
+         * @returns {Date} date from timestamp
+         */
         jsonDateToDate(dateString) {
+            // removes symbols and string from json date
             let timestamp = +dateString.replace(/\/Date\((.*?)\)\//g, "$1")
             return new Date(timestamp)
         },
 
+        /**
+         * Gets hours and minutes from json Date String
+         *
+         * @param {String} dateString The date string get hours and minutes from
+         * @returns {String} time in HH:MM
+         */
         startTime(dateString) {
             let programStartTime = this.jsonDateToDate(dateString)
             let h = programStartTime.getHours()
             let m = programStartTime.getMinutes()
-            // add a zero in front of numbers<10
             h = this.addZerosToSingleDigits(h)
             m = this.addZerosToSingleDigits(m)
             return h + ":" + m
         },
 
-        getDateFormatter(dateObj) {
+        /**
+         * Format date object to YYYY-MM-DD
+         *
+         * @param {Date} date to format
+         * @returns {String} YYYY-MM-DD
+         */
+        formatDate(dateObj) {
+            // +1 so jan is month 1 and not 0
             let month = dateObj.getUTCMonth() + 1
             let day = dateObj.getUTCDate()
             let year = dateObj.getUTCFullYear()
@@ -83,24 +102,36 @@ export default {
             return year + "-" + month + "-" + day
         },
 
-        sortArrayOnTime(arrayToSort) {
-            arrayToSort = arrayToSort.sort(function (a, b) {
-                console.log(a.channel.name + a.starttimeutc)
+        /**
+         * Sorts the program list on Time and then channel name
+         *
+         * @param {Array} programList to sort
+         * @returns {Array} the sorted array
+         */
+        sortProgramList(programList) {
+            programList = programList.sort(function (a, b) {
                 if (a.starttimeutc > b.starttimeutc) {
                     return 1
                 } else if (a.starttimeutc < b.starttimeutc) {
                     return -1
                 }
+                //compares the channel name if the times are the same
                 return a.channel.name.localeCompare(b.channel.name)
             })
-            return arrayToSort
+            return programList
         },
 
-        async getSingleTablo(api, tempArray) {
+        /**
+         * fetches tablo for one day
+         *
+         * @param {String} apiUrl url to fetch data from
+         * @param {Array} tempArray to push the result from the api into
+         */
+        async getSingleTablo(apiUrl, tempArray) {
             let json = null
 
             try {
-                let response = await fetch(api)
+                let response = await fetch(apiUrl)
                 if (!response.ok) {
                     throw new Error(response.status)
                 }
@@ -109,10 +140,12 @@ export default {
                 console.error(err)
             }
 
+            // schedule is the array with all programs
             json = json.schedule
 
             let dateNow = new Date()
 
+            //removes all programs from the list that has already aired
             for (let i = json.length - 1; i >= 0; i--) {
                 let dateProgram = this.jsonDateToDate(json[i].endtimeutc)
                 if (dateNow > dateProgram) {
@@ -120,42 +153,59 @@ export default {
                 }
             }
 
+            // Merge the arrays
             Array.prototype.push.apply(tempArray, json)
         },
 
-        async fillUpChanneltoProgramList(channelId) {
+        /**
+         * adds programs to the program list
+         *
+         * @param {String} channelId the channels id
+         */
+        async addProgramsToProgramList(channelId) {
             let date = new Date()
             let tempArray = []
 
-            let query = `channelid=${channelId}&date=${this.getDateFormatter(date)}&size=500&format=json`
+            // the query to add to the request
+            let query = `channelid=${channelId}&date=${this.formatDate(date)}&size=500&format=json`
 
+            // calls getSingleTablo one day at a time until number of programs is met
             do {
                 await this.getSingleTablo(`https://api.sr.se/api/v2/scheduledepisodes?${query}`, tempArray)
                 date.setDate(date.getDate() + 1)
             } while (tempArray.length < this.numberOfProgramsToShow)
 
+            //merge arrays
             Array.prototype.push.apply(this.programArrayTemp, tempArray)
 
-            this.programArrayTemp = this.sortArrayOnTime(this.programArrayTemp)
+            //Sorts the array
+            this.programArrayTemp = this.sortProgramList(this.programArrayTemp)
         },
 
-        updateAllChannels() {
+        /**
+         * Update the programlist for each radiostation that is checked
+         */
+        filterSelectedChannels() {
             this.programArrayTemp = []
             let checkBoxes = document.querySelectorAll(`input[type="checkbox"]`)
 
+            // loops all radio stations and adds programs to programlist for each checked radiostation
             checkBoxes.forEach((station) => {
                 if (station.checked) {
-                    this.fillUpChanneltoProgramList(station.parentNode.id)
+                    this.addProgramsToProgramList(station.parentNode.id)
                 }
             })
             this.allPrograms = this.programArrayTemp
         },
     },
+    //on mount get programs for pre checked channels
     mounted() {
-        this.updateAllChannels()
+        this.filterSelectedChannels()
     },
     computed: {
+        // runs function when the variable allPrograms is updated
         showCertainNumberOfPrograms() {
+            //  return new array with just the first indexes from 0 to numberOfProgramsToShow
             return this.allPrograms.slice(0, this.numberOfProgramsToShow)
         },
     },
