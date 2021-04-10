3c75faf0311eb890d6bc9ba9a879169afaaa7f73
Emma Fredriksson <emfr87@gmail.com>
2021-04-10 15:35:32 +0200

    Change calendar to a datepicker

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,33 @@
+<template>
+    <datepicker placeholder="Välj datum" v-model="$parent.inputDate" :lower-limit="startDate" :upper-limit="today" />
+</template>
+
+<script>
+import Datepicker from "vue3-datepicker"
+export default {
+    name: "DatePicker",
+    components: {
+        Datepicker,
+    },
+    props: {
+        startDate: String,
+        today: String,
+    },
+}
+</script>
+
+<style>
+input[type="text"] {
+    height: 3em;
+    width: 15em;
+    cursor: pointer;
+    border: 1px solid #84c1cc;
+    box-sizing: border-box;
+    border-radius: 10px;
+
+    font-family: Arimo;
+    color: #145f6d;
+    font-weight: bolder;
+    text-align: center;
+}
+</style>
